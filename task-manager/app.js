b206c741d0d034fedeadfba08d0b49d69b8acd93
Paul DiDonato <pjdidonato@galico.domainstacks.net>
2022-11-24 12:37:26 -0500

    push me

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const router = express.Router();
 const app = express()
-app.use(express.static('./public'))
-const port = 8080
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`)
+
+app.get('/hello', (req, res) => {
+  res.send('task manager app')
 })
+const port = 3000
+app.listen(port, console.log(`listening on ${port}`))
+/*
+app.get('/api/v1/tasks')       - get all the tasks
+app.post('/api/v1/tasks')      - create a new tasks
+app.get('/api/v1/tasks/:id')   - get single task
+app.patch('/api/v1/tasks/:id)  - update task
+app.delete('/api/v1/tasks/:id) - delete tasks
+*/
+
+
