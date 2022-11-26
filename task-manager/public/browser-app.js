6c40f89aed9b2c46ac6a782c04e94ad6a70e1588
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2022-11-26 14:16:56 -0500

    push me

--- task-manager/public/browser-app.js
+++ task-manager/public/browser-app.js
@@ -21,9 +21,6 @@ const showTasks = async () => {
         return `<div class="single-task ${completed && 'task-completed'}">
 <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
 <div class="task-links">
-
-
-
 <!-- edit link -->
 <a href="task.html?id=${taskID}"  class="edit-link">
 <i class="fas fa-edit"></i>
